05b3f671ea9d0e3e9232d1a3473f8d8bac6a5101
Sharad-Banga <sharadiit03@gmail.com>
2025-09-16 23:16:24 +0530

    adding collections

---
+++ week4/objects2.js
@@ -0,0 +1,148 @@
+//OBJECT - COLLECTIONS OF PRIMITE
+//OBJECT -  key-value pair
+
+// making of object
+// let user = {
+//   name : "sharad",
+//   class  : " cse",
+//   roll:383,
+//   isTall : true
+// }
+
+//prinitng of object
+// console.log(user);
+
+
+//accessing values of keys in an object
+
+// console.log(user.name);
+// console.log(user.class);
+
+// console.log(user["class"]);
+
+
+//**ADDING NEW KEY-VALUE PAIR */
+
+// user.roll = 366;
+
+// console.log(user);
+
+
+//delete a pair in object
+
+// delete user.class;
+
+// console.log(user);
+
+
+
+// let car = {
+
+//   name :"tesla",
+//   model : "plaid",
+//   color : "red",
+//   isAIDriven : true,
+
+// }
+
+
+// car["age old"] = 4;
+
+// primitives
+// let username = "sharad";
+// let password = 123;
+
+
+//json or object
+
+// let loginUser = {
+//   username : username,
+//   password : pass
+// }
+
+// let loginUser = {
+//   username,
+//   password : pass
+// }
+
+
+
+
+// FOR IN LOOP
+
+// let car = {
+//   name : "tesla",
+//   model : "model s",
+//   color  : "cherry",
+//   wheelDriven : "rare"
+// }
+
+// // for(let i in car){
+// //   console.log(i , " : ", car[i]);
+// // }
+
+
+// let a = "sharad";
+
+// let b = a;
+
+
+// // console.log(`a`,a);
+// // console.log(`b`,b);
+
+// a = " rahul";
+
+
+// console.log(`a`,a);
+// console.log(`b`,b);
+
+
+
+// let user = {
+//   name : "sharad"
+// }
+
+// let admin = user;
+ 
+// user.name = " rahul";
+
+// console.log(admin);
+
+
+
+//DEEP CLONING
+
+// let user = {
+//   name : "sharad",
+//   class : "cse",
+//   roll : 83
+// }
+
+// let clone = {};
+
+// // clone.name = "sharad";
+// //clone.key = value
+// console.log(clone);
+
+// for(let i in user){
+//   clone[i] = user[i];
+// }
+
+
+// // console.log(clone);
+// user.name = "rahul";
+// console.log(user);
+// console.log(clone);
+
+
+
+// let a = {};
+// let b = a;
+
+// console.log(a==b);
+
+
+let a = {};
+let b = {};
+
+console.log(a==b);
